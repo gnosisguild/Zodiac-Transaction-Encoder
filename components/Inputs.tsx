6321dcbc41b3272b0fb478f90d9d1804be9c3142
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-12-06 14:06:31 +0100

    cleanup

--- components/Inputs.tsx
+++ components/Inputs.tsx
@@ -16,7 +16,6 @@ const Inputs = ({ fn, inputValues, onChange }: Props) => (
       {fn.inputs.map((input, i) => {
         const id = inputId(fn, input, i)
         const error = inputValues[id]?.value && !inputValues[id]?.isValid
-        console.log(error)
         return (
           <li key={id}>
             <StackableContainer inputContainer>
