82a5bc5925ee93c816172a3605159e5736a85418
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-12-14 16:02:56 +0100

    show params for overloaded functions

--- components/FunctionSelect.tsx
+++ components/FunctionSelect.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Select from './Select'
-import { Interface } from '@ethersproject/abi'
+import { FunctionFragment, Interface } from '@ethersproject/abi'
 
 import StackableContainer from './StackableContainer'
 
@@ -9,11 +9,37 @@ type Props = {
   onChange(method: string): void
 }
 
+const FunctionOption: React.FC<{ abi: Interface; fn: string }> = ({
+  abi,
+  fn,
+}) => {
+  const functionFragment = abi.functions[fn]
+  const params = functionFragment.format('full').split('(')[1].split(')')[0]
+  const isOverloaded =
+    abi.fragments.filter(
+      (fragment) =>
+        fragment.type === 'function' && fragment.name === functionFragment.name
+    ).length > 1
+
+  return (
+    <div className="option">
+      {functionFragment.name} {isOverloaded && <small>({params})</small>}
+      <style jsx>{`
+        .option {
+          white-space: nowrap;
+          overflow-x: ellipsis;
+        }
+        
+      }`}</style>
+    </div>
+  )
+}
+
 const FunctionSelect = ({ abi, onChange }: Props) => {
   const createOptions = (abi: Interface) =>
     Object.keys(abi.functions).map((key) => ({
       value: key,
-      label: abi.functions[key].name,
+      label: <FunctionOption abi={abi} fn={key} />,
     }))
 
   const options = createOptions(abi)
